622e5dd0c92b5a64203c233dd90c3f58f91887de
Guilherme Pontes <guilherme.pontes@gmail.com>
2024-06-07 18:57:27 -0300

    Inseri arquivo php.ini

--- index.php
+++ index.php
@@ -13,6 +13,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
 
+    // Define variáveis padrão
+    $dat_compra = '2024-06-07';
+    $val_compra = 100;
+    $dat_validade = '2024-10-31';
+    $dsc_campanha = 'Dia dos Namorados';
+    $ind_uso = 'N';
+    $dat_uso = null;
+    $nom_paciente = null;
+    $cpf_paciente = null;
+    $cod_validacao = null;
+    $dsc_observacao = 'Inserido pelo PHP';
+
     // Conecta ao banco de dados
     try {
         $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
@@ -21,13 +33,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Prepara e executa o comando de inserção
         $stmt = $conn->prepare("
             INSERT INTO vivi.vouchers (
-                nom_comprador, eml_comprador, dat_compra, val_compra
+                nom_comprador,
+                eml_comprador,
+                dat_compra,
+                val_compra,
+                dat_validade,
+                dsc_campanha,
+                ind_uso,
+                dat_uso,
+                nom_paciente,
+                cpf_paciente,
+                cod_validacao,
+                dsc_observacao
             ) VALUES (
-                :nome, :email, '2024-06-07', '650'
+                :nom_comprador,
+                :eml_comprador,
+                :dat_compra,
+                :val_compra,
+                :dat_validade,
+                :dsc_campanha,
+                :ind_uso,
+                :dat_uso,
+                :nom_paciente,
+                :cpf_paciente,
+                :cod_validacao,
+                :dsc_observacao
             )
         ");
-        $stmt->bindParam(':nome', $nome);
-        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':nom_comprador', $nome);
+        $stmt->bindParam(':eml_comprador', $email);
+        $stmt->bindParam(':dat_compra', $dat_compra);
+        $stmt->bindParam(':val_compra', $val_compra);
+        $stmt->bindParam(':dat_validade', $dat_validade);
+        $stmt->bindParam(':dsc_campanha', $dsc_campanha);
+        $stmt->bindParam(':ind_uso', $ind_uso);
+        $stmt->bindParam(':dat_uso', $dat_uso);
+        $stmt->bindParam(':nom_paciente', $nom_paciente);
+        $stmt->bindParam(':cpf_paciente', $cpf_paciente);
+        $stmt->bindParam(':cod_validacao', $cod_validacao);
+        $stmt->bindParam(':dsc_observacao', $dsc_observacao);
         $stmt->execute();
 
         // Define a mensagem de sucesso
